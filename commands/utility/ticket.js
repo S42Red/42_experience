d14672ce6e45b48fcffe5294132a859457e9cdf1
Arthur <arthur@macbook-pro-de-arthur.home>
2023-11-03 14:20:13 +0100

    Ajout ticket

---
+++ commands/utility/ticket.js
@@ -0,0 +1,25 @@
+
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ticket")
+    .setDescription('Créez un ticket pour contacter le staff')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+    .setDMPermission(false),
+  async execute(interaction) {
+    await interaction.reply({ content: "Prise en compte de ta demande, chargement en cours...", ephemeral: true });
+
+    try {
+		await interaction.channel.parent.children.create({
+        name: `Ticket de ${interaction.user.username}`,
+        type: ChannelType.GuildText,
+      });
+
+      await interaction.editReply({ content: "Ta demande de contact a bien été reçue 📨\nUn salon a été créé pour discuter avec le staff !" });
+    } catch (error) {
+      console.log(error);
+      await interaction.editReply({ content: "Nous rencontrons un bug, exceptionnellement tu peux mp un staff en lui précisant que tu n'as pas pu créer de ticket à cause des méchants développeurs du bot XD" });
+    }
+  },
+};
